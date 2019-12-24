04898eeced00b9fd4a725ed919119514a15cd37a
Kevin <kevinkwon1995@gmail.com>
2019-12-24 13:04:11 -0800

    Updates

---
+++ precache-manifest.480ab2c6adff2719fa938b3afe89f580.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b053b3ea92d7ddcf75794850b33bd149",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2fb68b6360a55afb6276",
+    "url": "/portfolio/static/css/main.0c0a77ae.chunk.css"
+  },
+  {
+    "revision": "d8dac47c020cac30cd45",
+    "url": "/portfolio/static/js/2.77b34f76.chunk.js"
+  },
+  {
+    "revision": "5ba8ceb64eea848335a496f62ab9063a",
+    "url": "/portfolio/static/js/2.77b34f76.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fb68b6360a55afb6276",
+    "url": "/portfolio/static/js/main.9cd17db9.chunk.js"
+  },
+  {
+    "revision": "8575fab3f3395c90d845",
+    "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  },
+  {
+    "revision": "3d3445ab63f3afe004c91da42472ea92",
+    "url": "/portfolio/static/media/hootsuite-logo.3d3445ab.png"
+  },
+  {
+    "revision": "1f58b4a0338c394f898dd114808d4b54",
+    "url": "/portfolio/static/media/orbis-logo.1f58b4a0.png"
+  },
+  {
+    "revision": "57c9da05069ec20a63f9fcd841a72ef9",
+    "url": "/portfolio/static/media/sap-logo.57c9da05.png"
+  }
+]);
