48f8efecf93cb9807cb24ce346db470fd0029d6a
Kevin Kwon <kevinkwon1995@gmail.com>
2020-05-13 19:52:17 -0700

    Updates

---
+++ precache-manifest.77832c476ef1ade900cd736f5d9c89b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004fa01c8bcea8c1b7f990228c95610f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69ec6406594e4be23949",
+    "url": "/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "825e70914c20dafdd57c",
+    "url": "/static/css/main.0c0a77ae.chunk.css"
+  },
+  {
+    "revision": "69ec6406594e4be23949",
+    "url": "/static/js/2.110b6b93.chunk.js"
+  },
+  {
+    "revision": "729937898630e839994162d5d9f8ce89",
+    "url": "/static/js/2.110b6b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "825e70914c20dafdd57c",
+    "url": "/static/js/main.231bce20.chunk.js"
+  },
+  {
+    "revision": "b775e3132e0930477ff9",
+    "url": "/static/js/runtime-main.17a0efde.js"
+  },
+  {
+    "revision": "64c5353ddfa9f6d949f58cd357fad1bc",
+    "url": "/static/media/hootsuite-logo.64c5353d.png"
+  },
+  {
+    "revision": "a34a0d3a52f7c1d94e6a97bcf1575d4f",
+    "url": "/static/media/orbis-logo.a34a0d3a.png"
+  },
+  {
+    "revision": "57c9da05069ec20a63f9fcd841a72ef9",
+    "url": "/static/media/sap-logo.57c9da05.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.11e3f9675c62585bbc6039e4fe7ec769.js"
+  "/precache-manifest.77832c476ef1ade900cd736f5d9c89b2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
